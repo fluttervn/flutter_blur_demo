84b8bdaaa4ac13f6aa124df0bdd04b4ef8a6266b
Triet Bui Minh <x3cafe@gmail.com>
2019-09-29 19:44:27 +0700

    Add copyright

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2019 - Bui Minh Triet - x3cafe@gmail.com
+ * Website: http://fluttervn.com
+ * Facebook: https://www.facebook.com/fluttervndev
+ * Github: https://github.com/fluttervn
+ * Medium: https://medium.com/fluttervn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 import 'package:flutter/material.dart';
 
 import 'blur_multiple_widgets.dart';
